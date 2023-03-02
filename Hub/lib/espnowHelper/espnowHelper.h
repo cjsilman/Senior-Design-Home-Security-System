235c278be6b2c8bbe39be7101b2cb2bf5798c36f
Chris Silman <silma007@d.umn.edu>
2023-03-02 12:43:30 -0600

    Added more defines

--- Hub/lib/espnowHelper/espnowHelper.h
+++ Hub/lib/espnowHelper/espnowHelper.h
@@ -2,12 +2,17 @@
 #define ESPNOW_HELPER_H
 
 #define HUB_OK 1
+#define HUB_ARM 2
+#define HUB_DISARM 3
 #define CAM_ERR -1
 #define CAM_OK 1
 #define CAM_RDY 2
 #define GB_ERR -1
 #define GB_OK 1
 #define GB_NOISE 2
+#define TMP_ERR -1
+#define TMP_OK 1
+#define TMP_RDY 2
 
 #include "esp_now.h"
 #include <string.h>
